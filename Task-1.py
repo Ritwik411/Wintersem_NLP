d91a162b936227e039d8a0921798af9d8fbf97c0
Ritwik Gupta <40793690+Ritwik411@users.noreply.github.com>
2019-12-06 16:43:04 +0530

    Create Task-1.py
    Friday 6th December,2019

---
+++ Task-1.py
@@ -0,0 +1,7 @@
+text1 = "Shakespeare game the world best of theatrics"
+text = """William Shakespeare (bapt. 26 April 1564 – 23 April 1616)[a] was an English poet, playwright, and actor, widely regarded as the greatest writer in the English language and the world's greatest dramatist.[2][3][4] He is often called England's national poet and the "Bard of Avon" (or simply "the Bard").[5][b] His extant works, including collaborations, consist of some 39 plays,[c] 154 sonnets, two long narrative poems, and a few other verses, some of uncertain authorship. His plays have been translated into every major living language and are performed more often than those of any other playwright."""
+//Souce : Wikipedia first paragraph on William Shakespeare
+text_hin = 'शेक्सपियर खेल दुनिया का सबसे अच्छा नाटकीयता'
+print(text1)
+print(text)
+print(text_hin)
